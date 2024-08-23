4eb73d701d71e6fcd601b2d573bcc70a089c767b
SimDeveloper-cripto <catapano.smn.2001@gmail.com>
2024-08-23 17:33:23 +0200

    Removed useless folder

---
+++ zig_app/Project/.zig-cache/o/d3b22239fdd73dbc79b8c7463ed62882/cimport.h.d
@@ -0,0 +1,11 @@
+cimport.o: \
+  C:/Users/catap/software/projects/Embedded-Domotic-System/zig_app/Project/.zig-cache/o/d3b22239fdd73dbc79b8c7463ed62882/cimport.h \
+  C:/Users/catap/software/projects/Embedded-Domotic-System/zig_app/Project/.zig-cache/o/d44caafe66630b350daaf6612a8fe45e/raygui.h \
+  C:/Users/catap/software/projects/Embedded-Domotic-System/zig_app/Project/.zig-cache/o/d44caafe66630b350daaf6612a8fe45e/raylib.h \
+  C:/Users/catap/AppData/Local/Microsoft/WinGet/Packages/zig.zig_Microsoft.Winget.Source_8wekyb3d8bbwe/zig-windows-x86_64-0.13.0/lib/include/stdarg.h \
+  C:/Users/catap/AppData/Local/Microsoft/WinGet/Packages/zig.zig_Microsoft.Winget.Source_8wekyb3d8bbwe/zig-windows-x86_64-0.13.0/lib/include/__stdarg___gnuc_va_list.h \
+  C:/Users/catap/AppData/Local/Microsoft/WinGet/Packages/zig.zig_Microsoft.Winget.Source_8wekyb3d8bbwe/zig-windows-x86_64-0.13.0/lib/include/__stdarg_va_list.h \
+  C:/Users/catap/AppData/Local/Microsoft/WinGet/Packages/zig.zig_Microsoft.Winget.Source_8wekyb3d8bbwe/zig-windows-x86_64-0.13.0/lib/include/__stdarg_va_arg.h \
+  C:/Users/catap/AppData/Local/Microsoft/WinGet/Packages/zig.zig_Microsoft.Winget.Source_8wekyb3d8bbwe/zig-windows-x86_64-0.13.0/lib/include/__stdarg___va_copy.h \
+  C:/Users/catap/AppData/Local/Microsoft/WinGet/Packages/zig.zig_Microsoft.Winget.Source_8wekyb3d8bbwe/zig-windows-x86_64-0.13.0/lib/include/__stdarg_va_copy.h \
+  C:/Users/catap/AppData/Local/Microsoft/WinGet/Packages/zig.zig_Microsoft.Winget.Source_8wekyb3d8bbwe/zig-windows-x86_64-0.13.0/lib/include/stdbool.h
